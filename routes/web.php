3f35aa54cb86267ab875cda0cde5b246e491e33e
WebcoderPartha <parthadev76@gmail.com>
2023-03-27 12:55:45 +0600

    admin authentication route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Auth\AuthController;
+
+Route::controller(AdminController::class)->middleware('admin')->group(function (){
+    Route::get('/dashboard', 'Dashboard')->name('dashboard');
+});
+
+Route::controller(AuthController::class)->group(function (){
+    Route::get('/', 'Login')->name('login');
+});
+
+
 
 
